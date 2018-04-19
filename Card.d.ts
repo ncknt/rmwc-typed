a175766a6f1ef0828dc0a045f987fc6d5b971715
Mike Rose <mxr@mutsys.com>
2018-04-19 07:18:51 -0700

    fixed errors in Card and Button and brought them into sync with rmwc. added missing dev dependencies. updated dependency versions.

--- Card.d.ts
+++ Card.d.ts
@@ -8,41 +8,28 @@ import {
 } from './Base'
 
 export interface CardProps extends SimpleBaseProps<Card> {
-    /** Use the cards dark theme. */
-    themeDark?: boolean;
+    stroked?: boolean;
 }
 
-export interface CardMediaItemProps extends SimpleBaseProps<CardMediaItem> {
-    /** Sets the media item height to 120px. */
-    oneDotFiveX?: boolean;
-    /** Sets the media item height to 160px. */
-    twoX?: boolean;
-    /** Sets the media item height to 240px. */
-    threeX?: boolean;
-}
-
-export interface CardTitleProps extends SimpleBaseProps<CardTitle> {
-    /** Make the title large. */
-    large?: boolean;
+export interface CardMediaProps extends SimpleBaseProps<CardMedia> {
+    square?: boolean;
+    sixteenByNine?: boolean;
 }
 
 export interface CardActionsProps extends SimpleBaseProps<CardActions> {
-    /** Allows for vertical alignment of actions. */
-    vertical?: boolean;
+    fullBleed?: boolean;
 }
 
 export interface CardActionProps extends SimpleBaseProps<CardAction> {
-    compact?: boolean;
+    icon?: boolean;
+    iconToggle: boolean;
 }
 
-
 export class Card extends Component<CardProps> { }
-export class CardTitle extends Component<CardTitleProps> { }
-export class CardPrimary extends Component<SimpleBaseProps<CardPrimary>> { }
-export class CardSubtitle extends Component<SimpleBaseProps<CardSubtitle>> { }
-export class CardSupportingText extends Component<SimpleBaseProps<CardSupportingText>> { }
-export class CardActions extends Component<CardActionsProps> { }
+export class CardPrimaryAction extends Component<SimpleBaseProps<CardPrimaryAction>> { }
 export class CardMedia extends Component<SimpleBaseProps<CardMedia>> { }
-export class CardMediaItem extends Component<SimpleBaseProps<CardMediaItem>> { }
-export class CardHorizontalBlock extends Component<SimpleBaseProps<CardHorizontalBlock>> { }
+export class CardMediaContent extends Component<SimpleBaseProps<CardMediaContent>> { }
+export class CardActions extends Component<CardActionsProps> { }
+export class CardActionButtons extends Component<SimpleBaseProps<CardActionButtons>> { }
+export class CardActionIcons extends Component<SimpleBaseProps<CardActionIcons>> { }
 export class CardAction extends Component<CardActionProps> { }
